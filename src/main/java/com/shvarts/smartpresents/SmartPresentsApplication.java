e8d68c8e65ac4d733c0716b7ea13f771a00d7736
Aleksandr Shvarts <shvartcaleksandr@gmail.com>
2024-12-29 02:41:50 +0300

    Add first version with only mobile communication

---
+++ src/main/java/com/shvarts/smartpresents/SmartPresentsApplication.java
@@ -0,0 +1,13 @@
+package com.shvarts.smartpresents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPresentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPresentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shvarts/smartpresents/SmartPresentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shvarts.smartpresents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPresentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
